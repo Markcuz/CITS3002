d170b19183d226bdc646c1c7efcb801e25311b60
kacrapi <chengywong14@hotmail.com>
2015-05-15 12:28:31 +0800

    bank.c
    still work in progress

---
+++ Project/Bank/bank.c
@@ -0,0 +1,20 @@
+#include <bank.h>
+
+
+int checkDeposit(eCent cent){
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+
+
+    if ((rv = getaddrinfo(ANALYST_NAME, ANALYSTPORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+}
