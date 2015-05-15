5662455f0bc89592fbe7b2663bc9b8c7f22b23de
Marcus <20495924@student.uwa.edu.au>
2015-05-15 13:41:23 +0800

    Reformat

---
+++ Project/include/comms.h
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define MAXBUFLEN 100
+
+int send(char* port, char* hostname, char* message);
+
+int receive(char* port, char* receivedMessage);
