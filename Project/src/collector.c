e9cc260d82293219d418f0fef2132f2dc262a2bb
BjornBjornson <mfitzay@gmail.com>
2015-05-18 18:17:31 +0800

    added SSL_CA &encrypt CA. Tweaked args for sending

--- Project/src/collector.c
+++ Project/src/collector.c
@@ -1,102 +1,57 @@
-#include "collector.h"
 
-int sending(int payment, int data, char* message) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
-    //need to check the director
-    if(checkDirector() != 0) {
-        //wait for a while? until director has an analyst?
-    }
-    
-    //add something to front of string to identify a data exchange
-    
-    /* somethign to put together encrypted stirng*/
-    //adding encryption from Collector to Director (CD)
-    addEncrypt_CD(message);
-    
-    //adding SSL from Collector to Director
-    addSSL_CD(message);
-    
-    //sending the string
-    if(sendData(DIRECTPORT, DIRECTOR_NAME, message)==0) {
-        //returning success
-        return 0;
-    }
-    else {
-        //returning failure
-        return 1;
-    }
-    return 0;
-}
+#include "comms.h"
 
-int buy_eCent(void) {
-    char* message = "request";
-    
-    //adding Encryption Collector to Bank
-    encrypt_CB(message);
-    
-    //add SSL Colelctor to bank
-    SSL_CB(message);
-    
-    //need to parse a string here
-    if(sendData(BANKPORT, BANK_NAME, message) == 0) {
-        //returning success
-        return 0;
-    }
-    else {
-        //returning failure
-        return 1;
-    }
-        return 0;
-}
+//defines for certain types
+#define TEMP_DATA 1;
+#define MASS_DATA 2;
 
-int checkDirector(int type) {
-    //something to send to Director to identify the request
-    char* message = "check";
-    
-    //adding encryption from Collector to Director (CD)
-    addEncrypt_CD(message);
-    
-    //adding SSL from Collector to Director
-    addSSL_CD(message);
-    
-    if(sendData(DIRECTPORT, DIRECTOR_NAME, message)==0) {
-        //returning success
-        return 0;
-    }
-    else {
-        //returning failure
-        return 1;
-    }
-    
-    return 0;
-}
+#define MAXBUFLEN 100
 
-//receiving the processed Data from the director
-int receivingData() {
+#define DIRECTORPORT "4950"
+#define BANKPORT "4951"
 
-    char* message;
-    
-    //receiving on the director to collector port wait until has received this data
-    receiveData(DIRECTPORT, message);
-    
-    decrypt_DC(message);
-    
-    ///printing for when done?
-    printf(message);
-    
-    return 0;
-}
+#define DIRECTOR_NAME "127.0.0.1"
+#define BANK_NAME "192.168.1.2"
 
-int receiveeCent(){
-    char* message;
-    
-    //waits for response from bank
-    receiveData(BANKPORT, message);
-    
-    decrypt_BC(message);
-    
-    storeeCent(message);
-    
-    return 0;
-}
+
+//Wallet wallet;
+
+int sending(int data, char* Software);//sends dataType and eCent to Director
+
+int buy_eCent(void); //buys (gains an eCent from the bank)
+
+int checkDirector(int dataType); //checks Director
+
+int receiving(void); //receive the data
+
+int encrypt_CA(char* message);
+
+int SSL_CA(char* message);
+
+int encrypt_CB(char* message);
+
+int SSL_CB(char* message);
+
+int encrypt_CD(char* message);
+
+int SSL_CD(char* message);
+
+int decrypt_BC(char* message);
+
+int decrypt_DC(char* message);
+
+//splits the message into to corresponding eCent and stores it for later use
+int storeeCent(char* message);
+
 
